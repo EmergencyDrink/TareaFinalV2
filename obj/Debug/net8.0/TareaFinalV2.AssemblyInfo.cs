b0067d9330e0f77a81fa4113e2b756816bfec5c3
EmergencyDrink <andy123cx@gmail.com>
2025-03-03 15:44:27 -0500

    main

--- obj/Debug/net8.0/TareaFinalV2.AssemblyInfo.cs
+++ obj/Debug/net8.0/TareaFinalV2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TareaFinalV2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e5e898557f2b4e2e4e14b271fee5e5aebd48052")]
 [assembly: System.Reflection.AssemblyProductAttribute("TareaFinalV2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TareaFinalV2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
